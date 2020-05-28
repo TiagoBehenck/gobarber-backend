e1cdba6023616c4ccddfb9ea1ecc4614135042cd
TiagoBehenck <tiago.behenck.santos@gmail.com>
2020-05-28 14:13:28 -0300

    :gear: Add validation in the routes with Celebrate

--- src/modules/users/infra/http/routes/users.routes.ts
+++ src/modules/users/infra/http/routes/users.routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 import multer from 'multer';
+
 import uploadConfig from '@config/upload';
 
 import UsersController from '../controllers/UsersController';
@@ -13,7 +15,17 @@ const userAvatarController = new UserAvatarController();
 
 const upload = multer(uploadConfig);
 
-usersRouter.post('/', usersController.create);
+usersRouter.post(
+  '/',
+  celebrate({
+    [Segments.BODY]: {
+      name: Joi.string().required(),
+      email: Joi.string().email().required(),
+      password: Joi.string().required(),
+    },
+  }),
+  usersController.create,
+);
 
 usersRouter.patch(
   '/avatar',
